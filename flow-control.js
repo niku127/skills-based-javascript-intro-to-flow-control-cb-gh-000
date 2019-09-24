d5795d7c49eaf5259f3826aa5c6240708f6b2b15
Niklas Lohmann <nilohmann@htw-saarland.de>
2019-09-24 14:19:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,7 +27,7 @@ return age >= 13 && age <= 19 ? 'You are a teenager':'You are not a teenager'
 
 function switchAge(age) {
 switch(age){
-case >= 13 && <= 19:
+case 13:
   return 'You are a teenager'
   break;
 default: return 'You have an age'
